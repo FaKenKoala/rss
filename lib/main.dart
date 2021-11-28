a8e1dee2b6417f16915adac43ac252ffe668c14d
lcp <lcp@qycloud.com>
2021-11-28 20:39:04 +0800

    feat: android widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:home_widget/home_widget.dart';
 import 'package:rss/presentation/home/home.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  HomeWidget.registerBackgroundCallback(backgroundCallback);
   runApp(const MyApp());
+}
+
+Future<void> backgroundCallback(Uri? uri) async {
+  if (uri?.host == 'update_counter') {
+    int _counter = 0;
+    await HomeWidget.getWidgetData('_counter', defaultValue: 0).then((value) {
+      _counter = value ?? 0;
+      _counter++;
+    });
+    await HomeWidget.saveWidgetData<int>('_counter', _counter);
+    await HomeWidget.updateWidget(
+        name: 'AppWidgetProvider', iOSName: 'AppWidgetProvider');
+  }
 }
 
 class MyApp extends StatelessWidget {
--- lib/presentation/home/page/home_page.dart
+++ lib/presentation/home/page/home_page.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_widgetkit/flutter_widgetkit.dart';
+import 'package:home_widget/home_widget.dart';
 import 'package:rss/application/value_objects/rss_widget_data.dart';
 import 'package:rss/presentation/rss_list/rss_list.dart';
 
@@ -13,22 +14,52 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+  @override
+  void initState() {
+    super.initState();
+    HomeWidget.widgetClicked.listen((Uri? uri) => loadData());
+    loadData();
+  }
+
+  void loadData() async {
+    await HomeWidget.getWidgetData<int>('_counter', defaultValue: 0)
+        .then((value) {
+      setState(() {
+        _counter = value ?? 0;
+      });
+    });
+  }
+
+  Future<void> updateAppWidget() async {
+    await HomeWidget.saveWidgetData<int>('_counter', _counter);
+    await HomeWidget.updateWidget(
+        name: 'AppWidgetProvider', iOSName: 'AppWidgetProvider');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Rss'),
+        title: Text('Rss: ${_counter}'),
       ),
       body: const RssListPage(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          WidgetKit.setItem('rss_widget_data',
-              jsonEncode(RssWidgetData('OK, Rss!')), 'group.com.wombat.rss');
-          WidgetKit.reloadAllTimelines();
+          test() {
+            WidgetKit.setItem('rss_widget_data',
+                jsonEncode(RssWidgetData('OK, Rss!')), 'group.com.wombat.rss');
+            WidgetKit.reloadAllTimelines();
+          }
+
+          setState(() {
+            _counter++;
+          });
+          updateAppWidget();
         },
         child: const Icon(
           Icons.widgets_outlined,
-          color: Colors.orange,
+          color: Colors.white,
         ),
       ),
     );
